7ae3d9d77bc8ec037385d2693bd9af476e91085b
risakanagawa <gw.09ltry@gmail.com>
2019-07-21 22:12:26 -0700

    add sign in/up modals

---
+++ src/components/SignIn.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
+function SignUp() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <p onClick={handleShow}>Log In</p>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Welcome to geckos-team-07!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Button
+            variant="outline-secondary"
+            size="lg"
+            block
+            onClick={handleClose}
+          >
+            Log in with Google
+          </Button>
+          <p className="horizon-line">OR</p>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicChecbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+            <Button variant="warning" size="lg" block type="submit">
+              Log in
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>Don’t have an account?Sign up</Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default SignUp;
